81a0db8032caf94d6a4461a492cc7c6ca790cc75
Ji Bo <jibo@edan.com>
2013-08-14 09:57:39 +0800

    add "\n"
    Signed-off-by: Ji Bo <jibo@edan.com>

--- init/install_driver.py
+++ init/install_driver.py
@@ -1,8 +1,10 @@
 import os
+from commands import getstatusoutput as getso
+from handle_error import handle
 from   config import IP_ADDRESS, NET_DRIVER_PATH,NET_DRIVER_NAME  
 
 def install_netdriver():
-    print 'intall net dirver...\n'
+    print 'intall NET Driver...\n'
     s,o = getso("cd %s && make clean && make install && modprobe" % (NET_DRIVER_PATH,NET_DRIVER_NAME))
     handle(s,o)
     s,o = getso("ifconfig eth0 %s" %IP_ADDRESS)
