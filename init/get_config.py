ecd09d9b4d817f5d33237d86e0d40d0f4df99e22
Ji Bo <jibo@edan.com>
2013-08-14 11:09:04 +0800

    modify ssh prompt message
    Signed-off-by: Ji Bo <jibo@edan.com>

--- init/get_config.py
+++ init/get_config.py
@@ -18,7 +18,7 @@ def clone_config():
     handle(s,o)
 
 def generate_sshkey():
-    print 'configure ssh-key\n'
+    print 'configure ssh-key password...\n'
     s,o = getso("ssh-keygen -t dsa -C %s -f ~/.ssh/%s"  %(EMAIL,EMAIL))
     handle(s,o)
 
