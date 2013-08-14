b931748855da205b1af25322e481afc82877d926
Ji Bo <jibo@edan.com>
2013-08-14 10:32:46 +0800

    fix install driver bug
    Signed-off-by: Ji Bo <jibo@edan.com>

--- init/install_driver.py
+++ init/install_driver.py
@@ -1,14 +1,16 @@
 import os
 from commands import getstatusoutput as getso
 from handle_error import handle
-from   config import IP_ADDRESS, NET_DRIVER_PATH,NET_DRIVER_NAME  
+from config import IP_ADDRESS, NET_DRIVER_PATH, NET_DRIVER_NAME  
 
 def install_netdriver():
     print 'intall NET Driver...\n'
-    s,o = getso("cd %s && make clean && make install && modprobe" % (NET_DRIVER_PATH,NET_DRIVER_NAME))
+    
+    s,o = getso("cd %s && make clean && make install && modprobe %s  " %(NET_DRIVER_PATH,NET_DRIVER_NAME))
     handle(s,o)
-    s,o = getso("ifconfig eth0 %s" %IP_ADDRESS)
-
+    
+    s,o = getso("ifconfig eth0 %s " %IP_ADDRESS)
+    handle(s,o)
 
 def do():
     install_netdriver()
